b1963bb22d3bb8b046308a80a4cbf6dfbe4ed2bf
unknown <lekkalakarthik451@gmail.com>
2025-05-11 05:09:58 +0530

    added dummy secret

---
+++ test_secret.py
@@ -0,0 +1,5 @@
+# WARNING: This is a dummy AWS secret key for testing secret scanning.
+# Do not use real credentials in production code.
+
+aws_access_key_id = "AKIA1234567890EXAMPLE"
+aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
